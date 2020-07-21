00338be0b3ce1b3a18b676038aac1af29016a2fe
Eric H <4438015+eric-h-st@users.noreply.github.com>
2020-07-21 16:26:30 -0400

    Initial

---
+++ MMM-AmbientLights.js
@@ -0,0 +1,41 @@
+/* global Module */
+
+/* Magic Mirror
+* Module: MMM-PIR-Sensor
+*
+* By Paul-Vincent Roll http://paulvincentroll.com
+* MIT Licensed.
+*/
+
+Module.register('MMM-PIR-Sensor',{
+	defaults: {
+		sensorPin: 22,
+		sensorState: 1,
+		relayPin: false
+  },
+  
+  // Override socket notification handler.
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === 'USER_PRESENCE') {
+			this.userPresence = payload;
+			this.sendNotification(notification, payload);
+			if  (payload === false && this.config.powerSavingNotification === true){
+				this.sendNotification("SHOW_ALERT",{type:"notification", message:this.config.powerSavingMessage});
+			}
+			this.updateDom();
+		} else if (notification === 'SHOW_ALERT') {
+			this.sendNotification(notification, payload)
+		}
+	},
+
+	notificationReceived: function (notification, payload) {
+		if (notification === 'SCREEN_WAKEUP') {
+			this.sendNotification(notification, payload)
+		}
+	},
+
+	start: function () {
+		this.sendSocketNotification('CONFIG', this.config);
+		Log.info('Starting module: ' + this.name);
+	}
+});
